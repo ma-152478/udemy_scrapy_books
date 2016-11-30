73bf231782d8669e38d716eecd0e966b04f1802e
ma-152478 <ma-152478@hs-weingarten.de>
2016-11-30 17:23:21 +0100

    fixed check for availability

--- udemy_scraper/udemy_scraper/spiders/books.py
+++ udemy_scraper/udemy_scraper/spiders/books.py
@@ -25,17 +25,17 @@ class BooksSpider(Spider):
     def parse_detail(self,response):
         name = response.xpath('.//*[@class="col-sm-6 product_main"]/h1/text()').extract_first()
         category = response.xpath('.//*[@class="breadcrumb"]/li[3]/a/text()').extract_first()
-        avail_string = response.xpath('.//*[@class="instock availability"]/text()').extract()
-        availability = avail_string[1].split()[0] +' '+ avail_string[1].split()[1]
+        availability_temp = response.xpath('.//*[@class="table table-striped"]/tr[6]/td/text()').extract_first()
+        if availability_temp.startswith("In stock"):
+            availability = "yes"
+        else:
+            availability = "no"        
         price = response.xpath('.//*[@class="table table-striped"]/tr[4]/td/text()').extract()
         temp_rating = response.xpath('.//*[@class="col-sm-6 product_main"]/p[3]/@class').extract()        
-        print "temp-rating:"
-        print temp_rating
         rating = temp_rating[0]
         rating = rating[12:]
         rating = rating.lower()
         rating = rating + " of five stars"
-        print rating
         image_url = response.xpath('.//*[@class="item active"]/img').extract_first()
         string_len = len(image_url)
         jpg_found = image_url.find("jpg")
